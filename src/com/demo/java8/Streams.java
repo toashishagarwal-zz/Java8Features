206dc34c6d3e9624525b66c487ceb45b173b26a7
Ashish Agarwal <Ashish.Agarwal@adp.com>
2015-06-26 13:59:37 +0530

    organize imports

--- src/com/demo/java8/Streams.java
+++ src/com/demo/java8/Streams.java
@@ -3,8 +3,6 @@ package com.demo.java8;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 class Worker {
 	private String firstName;
