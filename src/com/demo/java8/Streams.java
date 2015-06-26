0ef48a58f357820a7ace59160ffb12042712f192
Ashish Agarwal <Ashish.Agarwal@adp.com>
2015-06-26 13:22:18 +0530

    Streams example
    Streams example

---
+++ src/com/demo/java8/Streams.java
@@ -0,0 +1,70 @@
+package com.demo.java8;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+class Worker {
+	private String firstName;
+	private String lastName;
+	private int age;
+	
+	public Worker(String firstName, String lastName, int age) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		return " firstName=" + firstName + ", lastName=" + lastName
+				+ ", age=" + age + "";
+	}
+}
+
+public class Streams {
+	public static void main(String[] args) {
+		List<Worker> workers = new ArrayList<Worker>();
+		workers.add(new Worker("Mukesh", "Rana", 14));
+		workers.add(new Worker("Randeep", "Huda", 16));
+		workers.add(new Worker("Zaid", "Khan", 10));
+		workers.add(new Worker("Jimmy", "Shergill", 11));
+		workers.add(new Worker("Nikhil", "Dwivedi", 18));
+		workers.add(new Worker("Rahul", "Bose", 21));
+		
+		// Get all child workers. Display their names alphabetically
+		// A child worker is the one whose age is less than or equal to 14 years
+		workers.stream()
+			.filter(w -> (w.getAge() <= 14))
+			.sorted(Comparator.comparing(Worker::getFirstName))
+			.forEach(System.out::println);
+		
+		// Count the number of child workers
+		long numberOfChildWorkers = workers.stream()
+										.filter(w -> (w.getAge() <= 14))
+										.count();
+		System.out.println("Number of Child workers are --> " + numberOfChildWorkers);
+	}
+}
